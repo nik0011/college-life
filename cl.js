e49a7f65a4cfafaf1644fdd4f780cff77e0e56a5
nik001 <nikhiltakle055@gmail.com>
2023-10-19 23:58:59 +0530

    initials files

--- cl.js
+++ cl.js
@@ -2,6 +2,7 @@ var page = document.querySelector("#main");
 var crsr = document.querySelector("#cursor");
 
 page.addEventListener("mousemove", function (dets) {
+  console.log(dets);
   crsr.style.left = dets.clientX + "px";
   crsr.style.top = dets.clientY + "px";
   ease: Power3;
